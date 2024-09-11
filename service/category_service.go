e0d1502619460477a995f201c90dcf878b975723
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-11 11:23:30 +0700

    mock testing

--- service/category_service.go
+++ service/category_service.go
@@ -12,8 +12,8 @@ type CategoryService struct {
 
 func (service CategoryService) Get(id string) (*entity.Category, error) {
 	category := service.Repository.FindById(id)
-	if category != nil {
-		return category, errors.New(("category not found"))
+	if category == nil {
+		return nil, errors.New(("category not found"))
 	} else {
 		return category, nil
 	}
--- service/category_service_test.go
+++ service/category_service_test.go
@@ -1,3 +1,40 @@
 package service
 
-func TestCategoryService_Get
+import (
+	"golang-unit-test/entity"
+	"golang-unit-test/repository"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
+var categoryService = CategoryService{Repository: categoryRepository}
+
+func TestCategoryService_GetNotFound(t *testing.T) {
+	// program mock
+	categoryRepository.Mock.On("FindById", "1").Return(nil)
+
+	category, err := categoryService.Get("1")
+	assert.NotNil(t, err)
+	assert.Nil(t, category)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	category := entity.Category{
+		Id:   "1",
+		Name: "Laptop",
+	}
+
+	// mock on get 3
+	categoryRepository.Mock.On("FindById", "3").Return(category)
+
+	// get on mock FindById 3
+	result, err := categoryService.Get("3")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
+}
